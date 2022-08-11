d92d5b2248b94c3112c1b66e1f8ecf60985d9b48
Ian <ssianfilipe@gmail.com>
2022-08-11 00:10:29 -0300

    create POST route

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,25 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
-import WebSocket, { WebSocketServer } from "ws";
-import data from "./db.json" assert { type: "json" };
+import fs from "fs";
+import { WebSocketServer } from "ws";
+// import data from "./db.json" assert { type: "json" };
 
 const app = express();
-
 const router = express.Router();
+
+let data = fs.readFileSync("db.json");
+let json = JSON.parse(data);
 
 router.get("/messages", (request, response) => {
   return response.status(200).send(data);
 });
 
-router.post("/message", (request, response) => {
-  const body = request.body;
+router.post("/message", async (request, response) => {
+  const body = await request.body;
+  json.messages.push(body);
+  fs.writeFileSync("db.json", JSON.stringify(json));
+  // fs.appendFileSync("db.json", JSON.stringify(body));
   return response.send(body);
 });
 
